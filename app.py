2853d794fe0803842cd94ef7a81b4146e34c4b0a
Rexhep Krasniqi <xhepi@bone.vet>
2021-11-24 19:12:46 +0100

    extending templates

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     d = str(datetime.now()).split(" ")[1].split(".")[0]
-    return render_template("index.html", ora=d)
+    return render_template("done.html", ora=d, title="INDEX TITLE")
 
 app.run(host='0.0.0.0', port='8000', debug=True)
