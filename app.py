4e956a8a53976965add6e9200729a564ad21803d
Tringa <tringadesku@gmail.com>
2021-11-24 19:40:08 +0100

    todo file

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     d = str(datetime.now()).split(" ")[1].split(".")[0]
-    return render_template("done.html", ora=d, title="INDEX TITLE")
+    return render_template("todo.html", ora=d, title="INDEX TITLE")
 
 app.run(host='0.0.0.0', port='8000', debug=True)
